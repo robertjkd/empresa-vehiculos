abc9e370b729653cac5346925a25e7935352bc1b
Robert <chinoalejandrovega@gmail.com>
2025-05-18 19:42:53 -0400

    ci : add de css al proyecto

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./redux/store"; // Asegúrate de importar el store
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>

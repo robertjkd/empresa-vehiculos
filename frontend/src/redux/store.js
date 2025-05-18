2a6bf3136a19c6d7df9cff992ea983f3ebb3644d
Robert <chinoalejandrovega@gmail.com>
2025-05-17 23:34:52 -0400

    probando

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,11 @@
 // frontend/src/redux/store.js
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice"; // Ruta correcta
-import userReducer from "./slices/userSlice"; // Asegúrate de que el archivo existe
+//import userReducer from "./slices/userSlice"; // Asegúrate de que el archivo existe
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    users: userReducer, // Solo si es necesario
+    //users: userReducer, // Solo si es necesario
   },
 });
